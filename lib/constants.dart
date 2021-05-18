e877572d081605c0b1e6c3cd4351dc28198d95f7
charlieblinsday <charlieblindsay@gmail.com>
2021-05-18 16:49:24 +0100

    updated constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-EdgeInsets kPaddingContainer = EdgeInsets.symmetric(vertical: 40.0, horizontal: 15.0);
+EdgeInsets kPaddingContainer = EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0);
 EdgeInsets kPaddingHomeScreenWelcome = EdgeInsets.fromLTRB(0.0, 200.0, 0.0, 40.0);
 
 ///FontStyle
