ed7db43f2d8cde53b2703f2dbb160e9d77bcc43a
Charlie Lindsay <charlieblindsay@gmail.com>
2021-06-29 21:49:42 +0100

    added bottom nav bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,37 +8,26 @@ String welcomeMessage =
     'Well done on taking the first step on the path to getting better...';
 
 class HomeScreen extends StatefulWidget {
+  // final bool lessonButtonVisibility;
+  //
+  // HomeScreen({@required this.lessonButtonVisibility});
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
-  bool lessonButtonVisibility = true;
   int symptomIndex2;
   String frequency;
   String timeOfDay;
+  int bottomNavIndex = 0;
 
-  Future<void> lessonButtonVisibilityFunction() async{
-    final prefs = await SharedPreferences.getInstance();
-    final symptomIndex = prefs.getInt('symptomIndex');
-    final selectedFrequency = prefs.getInt('frequency');
-    final selectedTimeOfDay = prefs.getInt('timeOfDay');
-    if(symptomIndex == null || selectedFrequency == null || selectedTimeOfDay == null){
-      lessonButtonVisibility = false;
-    }
-    else{
-      lessonButtonVisibility = true;
-    }
-  }
-
-  Future<void> _getSymptomIndex() async{
+  Future<void> _getSymptomIndex() async {
     final prefs = await SharedPreferences.getInstance();
     setState(() {
-        symptomIndex2 = prefs.getInt('symptomIndex');
-        frequency = prefs.getString('frequency');
-        timeOfDay = prefs.getString('timeOfDay');
+      symptomIndex2 = prefs.getInt('symptomIndex');
+      frequency = prefs.getString('frequency');
+      timeOfDay = prefs.getString('timeOfDay');
     });
   }
 
@@ -46,93 +35,119 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    lessonButtonVisibilityFunction();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('img/image_1.png'),
-          fit: BoxFit.cover,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('img/image_1.png'),
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      padding: kPaddingContainer,
-      constraints: BoxConstraints.expand(),
-      child: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Better',
-                  style: kLogoText.copyWith(fontSize: 48.0),
+        padding: kPaddingContainer,
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Better',
+                    style: kLogoText.copyWith(fontSize: 48.0),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0.0, 200.0, 0.0, 40.0),
+                child: Text(
+                  'Welcome'.toUpperCase(),
+                  style: kMainText.copyWith(fontSize: 43.0),
                   textAlign: TextAlign.center,
                 ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0.0, 200.0, 0.0, 40.0),
-              child: Text(
-                'Welcome'.toUpperCase(),
-                style: kMainText.copyWith(fontSize: 43.0),
+              ),
+              Text(
+                welcomeMessage.toUpperCase(),
+                style: kMainText.copyWith(fontSize: 19.0),
                 textAlign: TextAlign.center,
               ),
-            ),
-            Text(
-              welcomeMessage.toUpperCase(),
-              style: kMainText.copyWith(fontSize: 19.0),
-              textAlign: TextAlign.center,
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Visibility(
-                    visible: lessonButtonVisibility,
-                    child: TextButton(
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
                         child: Text(
                           'See daily lesson'.toUpperCase(),
                           style: kMainText.copyWith(fontSize: 18.0),
                         ),
                         style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(Colors.grey),
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.grey),
                             foregroundColor:
-                            MaterialStateProperty.all(Colors.black)),
+                                MaterialStateProperty.all(Colors.black)),
                         onPressed: () {
                           _getSymptomIndex();
                           Navigator.push(context,
                               MaterialPageRoute(builder: (context) {
-                            return LessonScreen(symptomIndex:symptomIndex2, frequency: frequency, timeOfDay: timeOfDay,);
+                            return LessonScreen(
+                              symptomIndex: symptomIndex2,
+                              frequency: frequency,
+                              timeOfDay: timeOfDay,
+                            );
                           }));
                         }),
-                  ),
-                  TextButton(
-                    child: Text(
-                      'Screen yourself'.toUpperCase(),
-                      style: kMainText.copyWith(fontSize: 18.0),
+                    TextButton(
+                      child: Text(
+                        'Screen yourself'.toUpperCase(),
+                        style: kMainText.copyWith(fontSize: 18.0),
+                      ),
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.grey),
+                          foregroundColor:
+                              MaterialStateProperty.all(Colors.black)),
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return QuizScreen();
+                        }));
+                      },
                     ),
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(Colors.grey),
-                        foregroundColor:
-                            MaterialStateProperty.all(Colors.black)),
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        return QuizScreen();
-                      }));
-                    },
-                  ),
-                ],
-              ),
-            )
-          ],
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
-    ));
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: bottomNavIndex,
+        onTap: (index){
+          setState(() {
+            bottomNavIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'Daily lesson'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.view_module),
+              label: 'Courses'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.medical_services),
+              label: 'Screen yourself'),
+        ],
+      ),
+    );
   }
 }
